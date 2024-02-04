ba034a6b1b445759863aa2aa990d7fcef01abff1
Zouine2005 <zouinemohamade@gmail.com>
2024-02-04 19:05:00 +0100

    retour button

--- addEleveAdmin.php
+++ addEleveAdmin.php
@@ -163,7 +163,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             
 
         <div class="float-start mt-3">
-            <button class="btn btn-danger"><a href="acceuilAdmin.php" class="text-white">تسجيل الخروج</a></button>
+            <button class="btn btn-primary"><a href="acceuilAdmin.php" class="text-white">رجوع <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5"/>
+                   </svg></a></button>
         </div>
         <div style="display: flex;">
             <div class="form-group">
